8d771d86b1f047b9dd7b04380645df60406762df
Sergi Ortiz Gómez <drualcman@msn.com>
2022-02-15 18:05:26 +0800

    add validation in client side

--- NorthWind.Sales.BlazorClient/_Imports.cs
+++ NorthWind.Sales.BlazorClient/_Imports.cs
@@ -1,4 +1,8 @@
 ﻿global using NorthWind.Sales.BusinessObjects.DTOs.CreateOrder;
-global using System.Net.Http.Json;
 global using Microsoft.AspNetCore.Components;
 global using NorthWind.Sales.WebApiGateway;
+global using Microsoft.AspNetCore.Components.Web;
+global using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+global using NorthWind.Entities.Interfaces;
+global using NorthWind.Sales.BlazorClient;
+global using NorthWind.Sales.Validalitor.CreateOrder;
